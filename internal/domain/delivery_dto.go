49ac8c8693e308f0af4e2aa26a51186a68daba89
BachWe <bahbabahbipam@gmail.com>
2026-06-16 07:24:31 +0300

    add del_dto

--- internal/domain/delivery_dto.go
+++ internal/domain/delivery_dto.go
@@ -16,7 +16,7 @@ type DeliveryResponse struct {
 	OrderID       string         `json:"order_id"`
 	CourierID     string         `json:"courier_id"`
 	Status        DeliveryStatus `json:"status"`
-	DeliveryPrice float64        `'json:"deliveryprice"'`
+	DeliveryPrice float64        `'json:"delivery_price"'`
 	PickedUpAt    *time.Time     `json:"picked_up_at"`
 	DeliveredAt   *time.Time     `json:"delivered_at"`
 }
